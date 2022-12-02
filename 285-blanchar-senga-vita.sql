6dd9fa27edb6fdf9b748636f4959e8460614527c
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2022-12-02 10:24:17 +0100

    first commit

---
+++ 285-blanchar-senga-vita.sql
@@ -0,0 +1,21 @@
+#devoir : créer des enoncé avec les conditions de l'exercice 3
+
+#1 Afficher la liste des professeurs dont le salaire est compris entre 2360000 et 2600000.
+
+#2 Afficher la liste des professeurs dont le salaire de base n'est pas 2360000.
+
+#3 Afficher la liste des élèves dont le nom contient un "a"
+
+#4 Afficher la liste des activités pratiquées qui ne sont pas au niveau 1.
+
+#5 #2 Afficher la liste des professeurs dont le salaire de base est 2000000.
+
+#6 Afficher la liste des élèves dont le nom fini par un Y
+
+#7 Afficher la liste des cours qui ont moins de 60 heures.
+
+#8 Afficher la liste des élèves qui ont un poids égale ou plus grand que 38
+
+|#9 Afficher la liste des activités dont la spécialité est mini foot et surf.
+
+#10 Afficher la liste des élèves qui ont un poids égale ou plus grand que 38
---
+++ hepl-pet-care-2/core/Sites/HEPLPetCare/Controllers/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace SGBD\Sites\HEPLPetCare\Controllers;
+
+use \SGBD\App;
+use \SGBD\Sites\HEPLPetCare\Models\Animal;
+
+/**
+* Cette classe gère l'affichage de la 
+* page d'accueil
+*/
+class HomeController
+{
+    public function show()
+    {
+//        var_dump(Animal::listHomepageAnimals()); die();
+        return App::getView('home', [
+            'site_name' => 'HEPL Pet Care',
+            'animals' => (Animal::listHomepageAnimals()),
+        ]);
+    }
+}
